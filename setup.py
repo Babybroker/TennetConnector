d64dc16755176c79a9db5f8703a9d20975a42cae
kevin <kevin.d@live.nl>
2022-08-15 21:16:02 +0200

    Create a datetime column

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tennetConnector',
-    version='0.3.0',
+    version='0.3.1',
     description='A python API wrapper for TenneT',
 
     # url='https://github.com/EnergieID/entsoe-py',
